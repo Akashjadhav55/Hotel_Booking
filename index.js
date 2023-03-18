6de3d2fa043f56345632093e33698a97e4f99236
Akashjadhav55 <akashjadhav7040253178@gmail.com>
2023-03-18 12:48:51 +0530

    validation

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 
+
 let signup = document.querySelector(".signup");
 let login = document.querySelector(".login");
 let slider = document.querySelector(".slider");
 let formSection = document.querySelector(".form-section");
 let LoginButton = document.getElementById("login-button")
 let SingupButton = document.getElementById("singup-button") 
+
+
+
+let LoginEmail = document.getElementById("email") 
+let LoginPassword = document.getElementById("password") 
+
+let SingupName = document.getElementById("sign-name") 
+let SingupEmail = document.getElementById("sign-email") 
+let SingupPassword = document.getElementById("sign-password") 
+let SingupComfirmPassword = document.getElementById("confirm-password") 
+
 
 signup.addEventListener("click", () => {
     slider.classList.add("moveslider");
@@ -17,6 +29,36 @@ login.addEventListener("click", () => {
 });
 
 
-LoginButton.addEventListener("click",() => {
-    Notiflix.Notify.success('Sol lucet omnibus');
+// LoginButton.addEventListener("click",() => {
+
+//     if()
+//     toastr["success"]("My name is Inigo Montoya. You killed my father. Prepare to die!")
+// })
+
+
+
+SingupButton.addEventListener("click", () => {
+
+    if(SingupPassword.value !== SingupComfirmPassword.value){
+        toastr["warning"]("password doesn't match?")
+    }
+    else if(SingupName.value == "" && SingupEmail.value =="" && SingupPassword.value == "" && SingupComfirmPassword.value == ""){
+        toastr["error"]("required all filled")
+    }
+    else{
+        let data = {
+            name : SingupName.value,
+            email : SingupEmail.value,
+            password : SingupPassword.value,
+        }
+       fetch("",{
+        method : "POST",
+        body : JSON.stringify({data}),
+        headers : {
+            "Content-type" : "application/json"
+        }
+
+       })
+    }
+    
 })
